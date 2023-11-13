78c91270e3ede329e19c61f5f4db6a71deb426e0
nh0126 <nh0126@sookmyung.ac.kr>
2023-11-14 00:12:22 +0900

    실습5

--- main.c
+++ main.c
@@ -2,11 +2,26 @@
 #include <string.h>
 
 void main(int argc, char *argv[])
-{
-	char str[30] = "happy C programming";
+{	FILE* fp;
+	char str[100];
+	int i;
 	
-	 printf("문자열 \"%s\"의 길이 : %i", str, strlen(str));
-
+	//1. OPEN FILE
+	fp = fopen("sample.txt","w");
+	
+	//2. WRITE FILE
+	for(i=0;i<3;i++)
+	{
+	    //2-1. PRINT "input a word"]
+	    printf("input a word: ");
+	    //2-2. SCANF A STRING
+	    scanf("%s", str);
+	    //2-3. fprintf()
+	    fprintf(fp, "%s\n", str);
+	}
+	//3. CLOSE FILE
+	fclose(fp); 
+	
 	system("PAUSE");	
   	return 0;
 }
