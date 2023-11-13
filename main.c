2953791b214402aa908af628d809fcaac11a512b
nh0126 <nh0126@sookmyung.ac.kr>
2023-11-14 00:14:23 +0900

    실습6

--- main.c
+++ main.c
@@ -3,24 +3,21 @@
 
 void main(int argc, char *argv[])
 {	FILE* fp;
-	char str[100];
-	int i;
+	char c;
 	
-	//1. OPEN FILE
-	fp = fopen("sample.txt","w");
-	
-	//2. WRITE FILE
-	for(i=0;i<3;i++)
-	{
-	    //2-1. PRINT "input a word"]
-	    printf("input a word: ");
-	    //2-2. SCANF A STRING
-	    scanf("%s", str);
-	    //2-3. fprintf()
-	    fprintf(fp, "%s\n", str);
-	}
-	//3. CLOSE FILE
-	fclose(fp); 
+	fp = fopen("sample.txt","r");//두가지 입력 (파일명,모드)
+  	if (fp == NULL)//sample.txt가 존재하지 않을 때 
+  	{
+  		printf("failed to open\n");
+        return 0;
+  	}
+
+  	while((c=fgetc(fp))!= EOF)//한 글자 읽은게 마지막이 아니면
+  	{
+	  putchar(c);//한글자 출력  
+  	}
+  	
+  	fclose(fp);
 	
 	system("PAUSE");	
   	return 0;
